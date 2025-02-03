3db7406a5de5ca471f671e38d7ea637fb0b49386
kase-haruto <0te06559726481t@au.com>
2025-02-03 12:07:47 +0900

    utf-8に修正

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 
 template <typename T>
 T Min(T val_1, T val_2){
-	//�������ق���Ԃ�
+	//小さいほうを返す
 	return (val_1 > val_2) ? val_2 : val_1;
 }
 
 template <>
 char Min<char>(char val_1, char val_2){
-	std::cout << "�����ȊO�͑���ł��܂���" << std::endl;
+	std::cout << "数字以外は代入できません" << std::endl;
 	return '\0';
 }
 
@@ -20,7 +20,7 @@ int main(){
 	std::cout << Min<float>(7.0f, 5.0f) << std::endl;
 	std::cout << Min<char>('a', 'b') << std::endl;
 
-	// �I���O�ɃL�[���͑҂���ǉ�
+	// 終了前にキー入力待ちを追加
 	std::cin.get();
 	return 0;
 }
